b3e42b1deafaf9c64af64c65bece69ff5756204b
hungpv <pvhung181.dev@gmail.com>
2026-09-06 13:26:13 +0700

    update localizations dependency

---
+++ lib/resources/l10n/app_localizations_en.dart
@@ -0,0 +1,55 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get themes => 'Themes';
+
+  @override
+  String get widgets => 'Widgets';
+
+  @override
+  String get icons => 'Icons';
+
+  @override
+  String get wallpapers => 'Wallpapers';
+
+  @override
+  String get minute => 'Minute';
+
+  @override
+  String get photo_refresh => 'Photo Refresh';
+
+  @override
+  String get show_temperature_in => 'Show temperature in';
+
+  @override
+  String get time_format => 'Time format';
+
+  @override
+  String get help => 'Help';
+
+  @override
+  String get languages => 'Languages';
+
+  @override
+  String get rate_app => 'Rate app';
+
+  @override
+  String get share_app => 'Share app';
+
+  @override
+  String get feedback => 'Feedback';
+
+  @override
+  String get privacy_policy => 'Privacy policy';
+
+  @override
+  String get settings => 'Settings';
+}
